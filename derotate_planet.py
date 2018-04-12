126673d7b586dbe04c3bc3cfdb4591ec48adadca
Christine Nguyen <nguyen.da.christine@gmail.com>
2018-04-11 17:00:58 -0700

    About to implement the use of seconds onto my program. I'm also hoping to create a shift projection method/class soon.

--- derotate_planet.py
+++ derotate_planet.py
@@ -5,13 +5,16 @@ import pyfits
 
 class DerotatePlanet:
 
-    def __init__(self, planet, H_coordgrid, K_coordgrid):
+    def __init__(self, planet, A_coordgrid, B_coordgrid):
         self.planet = planet
-        self.H_coordgrid = H_coordgrid
-        self.K_coordgrid = K_coordgrid
-        self.H_path = self.H_coordgrid.infile_path
-        self.K_path = self.K_coordgrid.infile_path
+        self.A_coordgrid = A_coordgrid
+        self.B_coordgrid = B_coordgrid
+        self.A_path = self.A_coordgrid.infile_path
+        self.B_path = self.B_coordgrid.infile_path
+        self.A_date, self.A_time = DerotatePlanet.get_date_and_time(self.A_path)
+        self.B_date, self.B_time = DerotatePlanet.get_date_and_time(self.B_path)
         self.degrees_per_minute = self.calculate_degrees_per_minute()
+
 
     def calculate_degrees_per_minute(self):
         """ Returns planet's rotation rate in longitudinal degrees per minute. """
@@ -22,7 +25,7 @@ class DerotatePlanet:
     def get_date_and_time(file):
         hdu = pyfits.open(file)
         headerdate = hdu[0].header['DATE-BEG']
-        return (DerotatePlanet.get_date(headerdate), DerotatePlanet.get_time(headerdate))
+        return DerotatePlanet.get_date(headerdate), DerotatePlanet.get_time(headerdate)
 
     def get_date(headerdate):
         return headerdate[0:10]
@@ -30,35 +33,49 @@ class DerotatePlanet:
     def get_time(headerdate):
         return headerdate[11:16]
 
-    def get_day_hour_minute(date, time):
+    def get_day_hour(date, time):
         day = int(date[8:10])
         hour = int(time[0:2])
-        minute = int(time[3:5])
-        return day, hour, minute
+        return day, hour
 
-    def get_time_in_order(H_date, H_time, K_date, K_time):
-        H_day, H_hour, H_minute = DerotatePlanet.get_day_hour_minute(H_date, H_time)
-        K_day, K_hour, K_minute = DerotatePlanet.get_day_hour_minute(K_date, K_time)
+    def get_minutes_seconds(time):
+        minutes = int(time[3:5])
+        seconds = int(time[6:8])
+        return minutes, seconds
 
-        if H_day == K_day:
-            if K_hour == H_hour:
-                if K_minute > H_minute:
-                    return H_date, H_time, K_date, K_time
+    def order_AB_date_time(A_date, A_time, B_date, B_time):
+        A_day, A_hour = DerotatePlanet.get_day_hour(A_date, A_time)
+        A_minutes, A_seconds = DerotatePlanet.get_minutes_seconds(A_time)
+        B_day, B_hour = DerotatePlanet.get_day_hour(B_date, B_time)
+        B_minutes, B_seconds = DerotatePlanet.get_minutes_seconds(B_time)
+
+        if A_day == 1:
+            A_day = 31
+            B_day = 30
+        if B_day == 1:
+            A_day = 30
+            B_day = 31
+
+        if A_day == B_day:
+            if B_hour == A_hour:
+                if B_minutes > A_minutes:
+                    return A_date, A_time, B_date, B_time
                 else:
-                    return K_date, K_time, H_date, H_time
-            if H_hour > K_hour:
-                return K_date, K_time, H_date, H_time
+                    return B_date, B_time, A_date, A_time
+            if A_hour > B_hour:
+                return B_date, B_time, A_date, A_time
             else:
-                return H_date, H_time, K_date, K_time
-        if H_day > K_day:
-            return K_date, K_time, H_date, H_time
+                return A_date, A_time, B_date, B_time
+        if A_day > B_day:
+            return B_date, B_time, A_date, A_time
         else:
-            return H_date, H_time, K_date, K_time
+            return A_date, A_time, B_date, B_time
 
-    def find_time_between_images(begin_date, begin_time, end_date, end_time):
-        begin_day, begin_hour, begin_minute = DerotatePlanet.get_day_hour_minute(begin_date, begin_time)
-        end_day, end_hour, end_minute = DerotatePlanet.get_day_hour_minute(end_date, end_time)
-        minute_difference = 0
+    def get_time_between_images(begin_date, begin_time, end_date, end_time):
+        begin_day, begin_hour = DerotatePlanet.get_day_hour(begin_date, begin_time)
+        begin_minutes, begin_seconds = DerotatePlanet.get_minutes_seconds(begin_time)
+        end_day, end_hour, end_minute = DerotatePlanet.get_day_hour(end_date, end_time)
+        end_minutes, begin_seconds = DerotatePlanet.get_minutes_seconds(begin_time)
 
         if begin_day == 1 and end_day == 31:
             begin_day = 31
@@ -73,13 +90,12 @@ class DerotatePlanet:
 
         if end_hour < begin_hour:
             end_hour = end_hour + 12
-        minute_difference = end_minute - begin_minute
+        minute_difference = (end_minutes + end_seconds/60) - (begin_minutes + begin_seconds/60)
         return 60 * abs(end_hour - begin_hour) + minute_difference
 
     def get_correction_time(self):
-        H_date, H_time = DerotatePlanet.get_date_and_time(self.H_path)
-        K_date, K_time = DerotatePlanet.get_date_and_time(self.K_path)
-        time_between_images = DerotatePlanet.find_time_between_images(H_date, H_time, K_date, K_time)
+        begin_date, begin_time, end_date, end_time = DerotatePlanet.order_AB_date_time(self.A_date, self.A_time, self.B_date, self.B_time)
+        time_between_images = DerotatePlanet.get_time_between_images(begin_date, begin_time, end_date, end_time)
         correction_time = time_between_images / 2
         return correction_time
 
--- derotate_planet_tests.py
+++ derotate_planet_tests.py
@@ -11,12 +11,6 @@ class TestDerotatePlanet(unittest.TestCase):
         self.planet = Neptune()
         date = '2017-08-31'
         self.H_path = Path(date, self.planet.name + '_H').all_files_in_folder[2]
-        """
-        
-        self.K_path = Path(date, planet.name + '_Ks').all_files_in_folder[2]
-        self.H_coordgrid = coordgrid.CoordGrid(self.H_path)
-        self.K_coordgrid = coordgrid.CoordGrid(self.K_path)
-        """
 
     def test_get_date_and_time(self):
         H_date, H_time = DerotatePlanet.get_date_and_time(self.H_path)
@@ -27,80 +21,80 @@ class TestDerotatePlanet(unittest.TestCase):
 
         # Test same hour
         H_date = K_date = '2017-08-31'
-        H_time, K_time = '10:40', '10:10'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        H_time, K_time = '10:40:02', '10:10:03'
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [K_date, K_time, H_date, H_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
         H_date = K_date = '2017-08-31'
-        H_time, K_time = '10:10', '10:40'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        H_time, K_time = '10:10:03', '10:40:10'
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [H_date, H_time, K_date, K_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
         # Test different hours
         H_date = K_date = '2017-08-31'
         H_time, K_time = '10:45', '09:50'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [K_date, K_time, H_date, H_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
         H_date = K_date = '2017-08-31'
         H_time, K_time = '10:45', '11:45'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [H_date, H_time, K_date, K_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
         # Test different days
         H_date, K_date = '2017-08-30', '2017-08-31'
         H_time, K_time = '11:45', '01:50'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [H_date, H_time, K_date, K_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
         H_date, K_date = '2017-08-31', '2017-08-30'
         H_time, K_time = '12:45', '11:50'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [K_date, K_time, H_date, H_time]
         self.assertEqual(order_list, test_list)
 
         # Test different months
         H_date, K_date = '2017-09-01', '2017-08-31'
         H_time, K_time = '02:45', '11:50'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [K_date, K_time, H_date, H_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
         H_date, K_date = '2017-08-31', '2017-09-01'
         H_time, K_time = '11:50', '02:45'
-        order_list = [DerotatePlanet.get_time_in_order(H_date, H_time, K_date, K_time)]
+        order_list = list(DerotatePlanet.order_AB_date_time(H_date, H_time, K_date, K_time))
         test_list = [H_date, H_time, K_date, K_time]
-        self.assertEqual(order_list, test_list)
+        self.assertListEqual(order_list, test_list)
 
     def test_get_time_between_images(self):
 
         # Test same hours
         begin_date = end_date = '2017-08-31'
         end_time, begin_time = '10:40', '10:10'
-        time_difference = DerotatePlanet.find_time_between_images(begin_date, begin_time, end_date, end_time)
+        time_difference = DerotatePlanet.get_time_between_images(begin_date, begin_time, end_date, end_time)
         self.assertEqual(time_difference, 30)
 
         # Test different hours
         begin_date = end_date = '2017-08-31'
         end_time, begin_time = '10:45', '09:50'
-        time_difference = DerotatePlanet.find_time_between_images(begin_date, begin_time, end_date, end_time)
+        time_difference = DerotatePlanet.get_time_between_images(begin_date, begin_time, end_date, end_time)
         self.assertEqual(time_difference, 55)
 
         # Test different days
         begin_date, end_date = '2017-08-30', '2017-08-31'
         begin_time, end_time = '11:45', '01:50'
-        time_difference = DerotatePlanet.find_time_between_images(begin_date, begin_time, end_date, end_time)
+        time_difference = DerotatePlanet.get_time_between_images(begin_date, begin_time, end_date, end_time)
         self.assertEqual(time_difference, 125)
 
         # Test different months
         begin_date, end_date = '2017-08-31', '2017-09-01'
         begin_time, end_time = '11:50', '02:45'
-        time_difference = DerotatePlanet.find_time_between_images(begin_date, begin_time, end_date, end_time)
+        time_difference = DerotatePlanet.get_time_between_images(begin_date, begin_time, end_date, end_time)
         self.assertEqual(time_difference, 175)
 
 if __name__ == '__main__':
