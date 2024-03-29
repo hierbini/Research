fe0ca891fff207c47872f797282b765dc89e6bf4
Christine Nguyen <37533383+hierbini@users.noreply.github.com>
2024-03-28 21:33:55 -0700

    Update get_ephem.py

--- get_ephem.py
+++ get_ephem.py
@@ -71,6 +71,7 @@ def get_ephemerides(code, obs_code, tstart, tend, stepsize) :
         with open('C:/Users/nguye/ResearchLab/Code/Nav/SavedEphemeris/' + combination, "rb") as file:
             print("Read data from file.")
             pickle.load("fuck you pickle. u are dangerous")
+            pickle.load("fuck you pickle. u are dangerous")
             dictionary = pickle.load(file)
             return dictionary["out"], dictionary["observatory_coords"]
     except FileNotFoundError:
